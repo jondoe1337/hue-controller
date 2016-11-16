89d3b27f2c0fe20c940caafbc8b43c0cfc412426
dada <dada@mutex.dadaurka.no-ip.org>
2016-11-16 20:03:58 +0100

    more cmds

--- hue-cmd/src/main/java/de/jondoe/hue/CommandLineInterface.java
+++ hue-cmd/src/main/java/de/jondoe/hue/CommandLineInterface.java
@@ -1,82 +1,135 @@
 package de.jondoe.hue;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
+import com.philips.lighting.model.PHLight;
 
-public class CommandLineInterface {
+public class CommandLineInterface
+{
 
-	private static final String EXIT = "exit";
-	private static final String HELP = "help";
-	private static final String DISCOVER = "discover";
-	private static final String CONNECT_LAST = "last";
-	private static final String CONNECT = "connect";
-	private HueCommands cmds;
+    private static final String EXIT = "exit";
+    private static final String HELP = "help";
+    private static final String DISCOVER = "discover";
+    private static final String CONNECT_LAST = "last";
+    private static final String CONNECT = "connect";
+    private static final String SHOW_LIGHTS = "showLights";
+    private static final String SET_LIGHT_STATE = "setLightState";
+    private static final String PATTERN_MAC = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$";
+    private HueCommands cmds;
 
-	public CommandLineInterface(HueCommands hueCommands) {
-		cmds = hueCommands;
-	}
+    public CommandLineInterface(HueCommands hueCommands)
+    {
+        cmds = hueCommands;
+    }
 
-	public void interact() {
-		try (Scanner scanner = new Scanner(System.in)) {
-			Splitter splitter = Splitter.on(' ').trimResults().omitEmptyStrings();
-			while (true) {
-				String inputString = scanner.next();
-				ArrayList<String> cmdList = Lists.newArrayList(splitter.split(inputString));
+    public void interact()
+    {
+        try (Scanner scanner = new Scanner(System.in))
+        {
+            Splitter splitter = Splitter.on(' ').trimResults().omitEmptyStrings();
+            while (true)
+            {
+                String inputString = scanner.next();
+                ArrayList<String> cmdList = Lists.newArrayList(splitter.split(inputString));
 
-				if (cmdList.isEmpty()) {
-					continue;
-				}
-				String cmd = cmdList.get(0);
-				switch (cmd) {
-				case HELP:
-					printHelp();
-					break;
-				case DISCOVER:
-					cmds.discoverBridges();
-					break;
-				case CONNECT_LAST:
-					connectToMostRecentBridge();
-					break;
-				case CONNECT:
-					connectToNewBrdige(cmdList);
-					break;
-				case EXIT:
-					return;
-				default:
-					defaultCmd(cmd);
-				}
-			}
-		}
-	}
+                if (cmdList.isEmpty())
+                {
+                    continue;
+                }
+                String cmd = cmdList.get(0);
+                switch (cmd)
+                {
+                    case HELP:
+                        printHelp();
+                        break;
+                    case DISCOVER:
+                        cmds.discoverBridges();
+                        break;
+                    case CONNECT_LAST:
+                        connectToMostRecentBridge();
+                        break;
+                    case CONNECT:
+                        connectToNewBridge(cmdList);
+                        break;
+                    case SHOW_LIGHTS:
+                        printLights();
+                        break;
+                    case SET_LIGHT_STATE:
+                        setLightState(cmdList);
+                        break;
+                    case EXIT:
+                        return;
+                    default:
+                        defaultCmd(cmd);
+                }
+            }
+        }
+    }
 
-	private void connectToNewBrdige(ArrayList<String> cmdList) {
-		if (cmdList.size() == 1) {
-			System.out.println("The Bridge-ID must be given!");
-		}
-	}
+    private void setLightState(ArrayList<String> cmdList)
+    {
+        if (cmdList.size() != 3)
+        {
+            throw new IllegalArgumentException("Light-ID, CommonColor and State must be given!");
+        }
+        cmds.setLightState(cmdList.get(0), cmdList.get(1), Boolean.parseBoolean(cmdList.get(2)));
+    }
 
-	private void connectToMostRecentBridge() {
-		boolean connected = cmds.connectToMostRecentBridge();
-		if (connected) {
-			System.out.println("Successful connected to most recent Hue bridge.");
-		} else {
-			System.out.println("No brdige configured recently - try 'connect'.");
-		}
-	}
+    private void printLights()
+    {
+        List<PHLight> allLights = cmds.getLights();
+        for (PHLight light : allLights)
+        {
+            System.out.println("Light: " + light.getIdentifier() + "  " + light.getName());
+        }
+    }
 
-	private void printHelp() {
-		System.out.println("Follwowing commands are given:");
-		System.out.println(EXIT + "\t- exit");
-		System.out.println(HELP + "\t- prints this help");
-		System.out.println(DISCOVER + "\t- starts discovery mode to find Hue bridges");
-		System.out.println(CONNECT_LAST + "\t- connects to most recent Hue bridge");
-		System.out.println("The Hue system is a registered trademark of Philips.");
-	}
+    private void connectToNewBridge(ArrayList<String> cmdList)
+    {
+        if (cmdList.size() != 1)
+        {
+            System.out.println("The Bridge-ID must be given!");
+        }
+        else
+        {
+            String macAddress = cmdList.get(0);
+            if (!macAddress.matches(PATTERN_MAC))
+            {
+                throw new IllegalArgumentException("Given ID is not a MAC-Address!");
+            }
+            cmds.connectToNewBridge(macAddress);
+        }
+    }
 
-	private void defaultCmd(String cmd) {
-		System.out.println("Unknown command:" + cmd);
-	}
+    private void connectToMostRecentBridge()
+    {
+        boolean connected = cmds.connectToMostRecentBridge();
+        if (connected)
+        {
+            System.out.println("Successful connected to most recent Hue bridge.");
+        }
+        else
+        {
+            System.out.println("No brdige configured recently - try 'connect'.");
+        }
+    }
+
+    private void printHelp()
+    {
+        System.out.println("Follwowing commands are given:");
+        System.out.println(EXIT + "\t- exit");
+        System.out.println(HELP + "\t- prints this help");
+        System.out.println(DISCOVER + "\t- starts discovery mode to find Hue bridges");
+        System.out.println(CONNECT_LAST + "\t- connects to most recent Hue bridge");
+        System.out.println("The Hue system is a registered trademark of Philips.");
+    }
+
+    private void defaultCmd(String cmd)
+    {
+        System.out.println("Unknown command:" + cmd);
+    }
 }
--- hue-cmd/src/main/java/de/jondoe/hue/HueCommands.java
+++ hue-cmd/src/main/java/de/jondoe/hue/HueCommands.java
@@ -7,41 +7,113 @@ import java.util.Map;
 import com.philips.lighting.hue.sdk.PHAccessPoint;
 import com.philips.lighting.hue.sdk.PHBridgeSearchManager;
 import com.philips.lighting.hue.sdk.PHHueSDK;
+import com.philips.lighting.hue.sdk.utilities.PHUtilities;
+import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHLight;
+import com.philips.lighting.model.PHLightState;
 
-public class HueCommands {
+public class HueCommands
+{
+    private enum CommonColors
+    {
+     WHITE(255, 255, 255);
 
-	private PHHueSDK phHueSDK = PHHueSDK.getInstance();
-	private Map<String, PHAccessPoint> macAddress2bridge = new HashMap<>();
+        private int r;
+        private int g;
+        private int b;
 
-	public void discoverBridges() {
-		PHBridgeSearchManager sm = (PHBridgeSearchManager) phHueSDK.getSDKService(PHHueSDK.SEARCH_BRIDGE);
-		sm.search(true, true);
-	}
+        private CommonColors(int r, int g, int b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
 
-	public boolean connectToMostRecentBridge() {
-		String username = HueProperties.getUsername();
-		String lastIpAddress = HueProperties.getLastConnectedIP();
+        public int getRed()
+        {
+            return r;
+        }
 
-		if (username == null || lastIpAddress == null) {
-			return false;
-		}
-		PHAccessPoint accessPoint = new PHAccessPoint();
-		accessPoint.setIpAddress(lastIpAddress);
-		accessPoint.setUsername(username);
-		phHueSDK.connect(accessPoint);
-		return true;
-	}
+        public int getGreen()
+        {
+            return g;
+        }
 
-	public void connectToNewBridge(String macAddress) {
-		PHAccessPoint accessPoint = macAddress2bridge.get(macAddress);
-		if (accessPoint == null) {
-			System.out.println("The bridge with the following MAC-address was not discovered: " + macAddress);
-		} else {
-			phHueSDK.connect(accessPoint);
-		}
-	}
+        public int getBlue()
+        {
+            return b;
+        }
+    }
 
-	public void registerFoundBridges(List<PHAccessPoint> accessPointsList) {
-		accessPointsList.forEach(ap -> macAddress2bridge.put(ap.getMacAddress(), ap));
-	}
+    private PHHueSDK phHueSDK = PHHueSDK.getInstance();
+    private Map<String, PHAccessPoint> macAddress2bridge = new HashMap<>();
+
+    public void discoverBridges()
+    {
+        PHBridgeSearchManager sm = (PHBridgeSearchManager) phHueSDK.getSDKService(PHHueSDK.SEARCH_BRIDGE);
+        sm.search(true, true);
+    }
+
+    public boolean connectToMostRecentBridge()
+    {
+        String username = HueProperties.getUsername();
+        String lastIpAddress = HueProperties.getLastConnectedIP();
+
+        if (username == null || lastIpAddress == null)
+        {
+            return false;
+        }
+        PHAccessPoint accessPoint = new PHAccessPoint();
+        accessPoint.setIpAddress(lastIpAddress);
+        accessPoint.setUsername(username);
+        phHueSDK.connect(accessPoint);
+        return true;
+    }
+
+    public void connectToNewBridge(String macAddress)
+    {
+        PHAccessPoint accessPoint = macAddress2bridge.get(macAddress);
+        if (accessPoint == null)
+        {
+            System.out.println("The bridge with the following MAC-address was not discovered: " + macAddress);
+        }
+        else
+        {
+            phHueSDK.connect(accessPoint);
+        }
+    }
+
+    public void registerFoundBridges(List<PHAccessPoint> accessPointsList)
+    {
+        accessPointsList.forEach(ap -> macAddress2bridge.put(ap.getMacAddress(), ap));
+    }
+
+    public List<PHLight> getLights()
+    {
+        PHBridge bridge = getSelectedBridge();
+        return bridge.getResourceCache().getAllLights();
+    }
+
+    private PHBridge getSelectedBridge()
+    {
+        PHBridge bridge = phHueSDK.getSelectedBridge();
+        if (bridge == null)
+        {
+            throw new IllegalStateException("No bridge selected!");
+        }
+        return bridge;
+    }
+
+    public void setLightState(String lightId, String commonColour, boolean on)
+    {
+        CommonColors color = CommonColors.valueOf(commonColour.toUpperCase());
+
+        PHLightState lightState = new PHLightState();
+        float xy[] = PHUtilities.calculateXYFromRGB(color.getRed(), color.getGreen(), color.getBlue(), "LCT001");
+        lightState.setX(xy[0]);
+        lightState.setY(xy[1]);
+        lightState.setOn(on);
+
+        phHueSDK.getSelectedBridge().updateLightState(lightId, lightState, null);
+    }
 }
